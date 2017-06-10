cca651d5f3bddd0cc482b83e49e3ff797cb36d5e
Dennis Ideler <ideler.dennis@gmail.com>
2017-06-10 23:54:08 +0100

    Add M.odules_all/0 to list all modules
    Lists every module that ships with Elixir. A bit too much since it also
    includes Exceptions and so on, but a good start for now.

--- .iex.exs
+++ .iex.exs
@@ -4,8 +4,33 @@ end
 
 defmodule Modules do
   def odules do
+    get_modules() |> filter_sort_erlang_modules
+  end
+
+  def odules_all do
+    get_modules_from_applications() |> filter_sort_erlang_modules
+  end
+
+  defp get_modules do
     :code.all_loaded
     |> Enum.map(&elem(&1, 0))
+  end
+
+  defp get_modules_from_applications do
+    for app <- loaded_applications(),
+        {:ok, modules} = :application.get_key(app, :modules),
+        module <- modules do
+      module
+    end
+  end
+
+  defp loaded_applications do
+    :application.loaded_applications
+    |> Enum.map(&elem(&1, 0))
+  end
+
+  defp filter_sort_erlang_modules(modules) do
+    modules
     |> Enum.filter(&(Regex.match?(~r/[A-Z]/, Atom.to_string(&1))))
     |> Enum.sort
   end
